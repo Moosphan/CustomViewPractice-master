7397bd145d95aac82514434c83e029a58377067a
suhao <2301230182@qq.com>
2018-03-07 10:01:57 +0800

    完成自定义view的练习三内容

---
+++ CustomViewPractice/app/src/main/java/com/moos/practice/activity/MainActivity.java
@@ -0,0 +1,55 @@
+package com.moos.practice.activity;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.moos.practice.R;
+
+/**
+ * by moos on 2018/03/04
+ */
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private Button button1_1,button1_2,button1_3;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        initView();
+
+    }
+
+    private void initView(){
+        button1_1 = findViewById(R.id.practice1_1);
+        button1_2 = findViewById(R.id.practice1_2);
+        button1_3 = findViewById(R.id.practice1_3);
+        button1_1.setOnClickListener(this);
+        button1_2.setOnClickListener(this);
+        button1_3.setOnClickListener(this);
+    }
+
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.practice1_1:
+                Intent intent1_1 = new Intent(this,Practice1_1Activity.class);
+                startActivity(intent1_1);
+                break;
+
+            case R.id.practice1_2:
+                Intent intent1_2 = new Intent(this,Practice1_2Activity.class);
+                startActivity(intent1_2);
+                break;
+
+            case R.id.practice1_3:
+                Intent intent1_3 = new Intent(this,Practice1_3Activity.class);
+                startActivity(intent1_3);
+                break;
+
+        }
+    }
+}
---
+++ CustomViewPractice/app/src/main/java/com/moos/practice/view/view1_3/Practice02StaticLayoutView.java
@@ -0,0 +1,44 @@
+package com.moos.practice.view.view1_3;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.support.annotation.Nullable;
+import android.text.Layout;
+import android.text.StaticLayout;
+import android.text.TextPaint;
+import android.util.AttributeSet;
+import android.view.View;
+
+public class Practice02StaticLayoutView extends View {
+    TextPaint textPaint = new TextPaint(Paint.ANTI_ALIAS_FLAG);
+    String text = "Hello\nHenCoder";
+
+    public Practice02StaticLayoutView(Context context) {
+        super(context);
+    }
+
+    public Practice02StaticLayoutView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public Practice02StaticLayoutView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    {
+        textPaint.setTextSize(60);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+
+        // 使用 StaticLayout 代替 Canvas.drawText() 来绘制文字，
+        // 以绘制出带有换行的文字
+        //canvas.drawText(text, 50, 100, textPaint);
+
+        StaticLayout staticLayout = new StaticLayout(text,textPaint,500, Layout.Alignment.ALIGN_NORMAL,1,0,true);
+        staticLayout.draw(canvas);
+    }
+}
